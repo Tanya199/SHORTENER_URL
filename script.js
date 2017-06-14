9cf052e18bc9bbd846ce5068ac4c263c451eb86f
tanya <tatka480@gmail.com>
2017-06-15 00:49:01 +0300

    add localStorage functionality

--- script.js
+++ script.js
@@ -1,32 +1,75 @@
 (function () {
-	var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-	var alphabet =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var upAlphabet =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	var alphabet = ["a", "b", "c", "d", "e", "f", "g",
+		"h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+		"r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	var upAlphabet = ["A", "B", "C", "D", "E", "F", "G",
+		"H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+		"S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 	var body = document.getElementsByTagName('body')[0];
 	var input = document.getElementById('input');
 	var buttonShorten = document.getElementById('btn_short');
 	var containerResults = document.getElementById('cont_result');
 
+	var arrayOfLinks = JSON.parse(localStorage.getItem('arrayOfLinks'));
+
+	if (arrayOfLinks && arrayOfLinks.length > 0) {
+		for (var i = 0; i < arrayOfLinks.length; i++) {
+			var wrapOriginalUrl = document.createElement('div');
+			var wrapNewUrl = document.createElement('div');
+			var buttonRemove = document.createElement('button');
+			buttonRemove.innerHTML = 'remove';
+			wrapOriginalUrl.innerHTML = arrayOfLinks[i].originalLink;
+			wrapNewUrl.innerHTML = arrayOfLinks[i].newLink;
+
+			containerResults.appendChild(wrapOriginalUrl);
+			containerResults.appendChild(wrapNewUrl);
+			containerResults.appendChild(buttonRemove);
+
+		}
+	}
+
 	buttonShorten.addEventListener('click', getNewLink);
 
+	function getRandomValueFromArray(array) {
+		var randomIndex = Math.floor(Math.random() * array.length);
+		return array[randomIndex];
+	}
+
 	function getNewLink() {
-		if(!input.value){
+		if (!input.value) {
 			return;
+
 		}
-		var wrapOriginalUrl = document.createElement('span');
-		var wrapNewUrl = document.createElement('span');
+		var wrapOriginalUrl = document.createElement('div');
+		var wrapNewUrl = document.createElement('div');
+		var buttonRemove = document.createElement('button');
+
+		var numberRandom = Math.floor(Math.random() * 10);
+		var randomLowerLetter = getRandomValueFromArray(alphabet);
+		var randomUpperLetter = getRandomValueFromArray(upAlphabet);
+
+		var linkObj = {
+			'originalLink': input.value,
+			'newLink': 'http://goo.gl/' + numberRandom + randomLowerLetter + randomUpperLetter
+		};
+
+		var arrayOfLinks = JSON.parse(localStorage.getItem('arrayOfLinks'));
+		if (arrayOfLinks && arrayOfLinks.length > 0) {
+			arrayOfLinks.push(linkObj);
+			localStorage.setItem('arrayOfLinks', JSON.stringify(arrayOfLinks));
+		} else {
+			localStorage.setItem('arrayOfLinks', JSON.stringify([linkObj]));
+		}
+
+		wrapOriginalUrl.innerHTML = linkObj.originalLink;
+		wrapNewUrl.innerHTML = linkObj.newLink;
+		buttonRemove.innerHTML = 'remove';
+
 		containerResults.appendChild(wrapOriginalUrl);
 		containerResults.appendChild(wrapNewUrl);
+		containerResults.appendChild(buttonRemove);
 
-		var originalLink = input.value;
-		var newLink ='goo.gl';
-
-		wrapOriginalUrl.innerHTML = originalLink;
-		wrapNewUrl.innerHTML = newLink;
-		// var re = /(\w+)\s(\w+)/;
-		// var str = "John Smith";
-		// var newstr = str.replace(re, "$2, $1");
-		// console.log(newstr);
 	}
 
 }());
